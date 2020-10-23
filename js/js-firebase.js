72f55838f201de5e17af79f046cc719d9b6f3b4d
RaihanMastermind729 <72647696+RaihanMastermind729@users.noreply.github.com>
2020-10-23 14:20:22 +0700

    Update js-firebase.js

--- js/js-firebase.js
+++ js/js-firebase.js
@@ -80,7 +80,7 @@
        // Isikan data kedalam firebase
        var tambahMember = dbRef_add_proses.ref("Member/" + id_add);
        tambahMember.set({
-         id_member : parseInt(id_add, RadixValue), 
+         id_member : parseInt(id_add, 10), 
          Nama : name_add,
          Email : email_add,
          No_telp : telp_add,
